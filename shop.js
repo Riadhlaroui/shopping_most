269c47197a47b3da90063480bc02797ddf7b1ab6
Riadhlaroui <143101405+Riadhlaroui@users.noreply.github.com>
2023-08-24 16:32:43 +0100

    Add files via upload

---
+++ shop.js
@@ -0,0 +1,10 @@
+let img = document.querySelector(".img");
+let container = document.querySelector(".contaner")
+function trans(bag){
+    img.src = bag;
+}
+function colors(color){
+     let a=container;
+     a.style=color;
+     
+}
